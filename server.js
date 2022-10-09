62062492559ba2f5f476aa2ce43305844022c7eb
Madwesh <mawesh.1155@gmail.com>
2022-10-09 21:35:33 +0530

    ESP32 interface

--- server.js
+++ server.js
@@ -1,45 +1,145 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({port: 8000, host : '192.168.0.108'});
+global.no_ESP = 0;
+
+const wss = new WebSocket.Server({
+  port: 8000,
+  host: '192.168.0.108'
+});
 
 const clients = new Map();
-
+var mobile = "false";
+var ESP = "false";
+var pc = "false";
+var copy = false;
 wss.on('connection', (ws) => {
-  ws.send(JSON.stringify({
-    conection:"true",
-    isfirst:"true",
-    cmd:"Request_device"
-  }));
-
+  //   ws.send(JSON.stringify({
+  //     conection:"true",
+  //     isfirst:"true",
+  //     cmd:"Request_device"
+  //   }));
   const id = uuidv4();
   var device;
 
   ws.on('message', (messageAsString) => {
     const message = JSON.parse(messageAsString);
-    if(message.isfirst == "true"){
-      device = message.device;
-      const metadata = {id, device};
-      clients.set(ws, metadata); 
-      console.log(device);
-      ws.send(JSON.stringify({
-        conection:"true"
-      }));
+    if (message.isfirst == "true") {
+      if (ispresent(message.device) && message.device != "ESP") {
+        ws.send(JSON.stringify({
+          conection: "alreadyConected",
+          devices: JSON.stringify({
+            mobile: mobile,
+            ESP: ESP,
+            pc: pc
+          })
+        }));
+        ws.close();
+        copy = true;
+      }else {
+        device = message.device;
+        const metadata = {
+          id,
+          device
+        };
+        if (device == "mobile") {
+          mobile = "true";
+
+        } else if (device == "ESP") {
+          global.no_ESP += 1;
+          console.log(global.no_ESP);
+          ESP = "true";
+
+        } else if (device == "pc") {
+          pc = "true";
+        }
+        [...clients.keys()].forEach((client) => {
+          client.send(JSON.stringify({
+    
+            conection: "newConnection",
+    
+            devices: JSON.stringify({
+              mobile: mobile,
+              ESP: ESP,
+              pc: pc
+            })
+    
+          }));
+        });
+        clients.set(ws, metadata);
+        console.log(device);
+
+        ws.send(JSON.stringify({
+          conection: "true",
+          devices: JSON.stringify({
+            mobile: mobile,
+            ESP: ESP,
+            pc: pc
+          })
+        }));
+      }
     }
   });
 
   ws.on("close", () => {
-    ws.send(JSON.stringify({
-      conection:"lost"
-    }));
-    clients.delete(ws);
+    if(!copy){
+      if (device == "mobile") {
+        mobile = "false";
+
+      } else if (device == "ESP") {
+        if(global.no_ESP<=1){
+          console.log(global.no_ESP);
+          ESP = "false";
+        }else{
+          global.no_ESP -= 1;
+          console.log(global.no_ESP);
+        }
+
+      } else if (device == "pc") {
+        pc = "false";
+
+      }
+      clients.delete(ws);
+      [...clients.keys()].forEach((client) => {
+        client.send(JSON.stringify({
+
+          conection: "lost",
+
+          devices: JSON.stringify({
+            mobile: mobile,
+            ESP: ESP,
+            pc: pc
+        })
+
+      }));
+    });
     console.log(device + " lost conection");
+  }else{
+    copy = false;
+  }
   });
+  ws.onerror = function () {
+    console.log("Some Error occurred");
+}
 })
 
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0,
+      v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
+}
+
+function ispresent(dev){
+  if(dev == "ESP"){
+    return ESP == "true";
+  }
+  if(dev == "pc"){
+    return pc == "true";
+  }
+  if(dev == "mobile"){
+    return mobile == "true";
+  }
+  return false;
 }
 
 console.log("wss up");
